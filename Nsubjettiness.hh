6d480aa001bbcc92aad61266695486f3ffaec89e
Jesse Thaler <jthaler@mit.edu>
2013-11-11 16:50:48 +0000

    Addressed some of the function name confusion in Njettiness
    git-svn-id: https://svn.hepforge.org/fastjetsvn/contrib/contribs/Nsubjettiness/trunk@380 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- Nsubjettiness.hh
+++ Nsubjettiness.hh
@@ -55,9 +55,7 @@ public:
    
    /// returns tau_N, measured on the constituents of this jet
    Double32_t result(const PseudoJet& jet) const;
-   // returns the normalization factor calculated in Njettiness --TJW
-   Double32_t d0(const PseudoJet& jet) const;
-   
+
    //ADDED BY TJW to set axes for manual use 
    void setAxes(std::vector<fastjet::PseudoJet> myAxes) {
       // TODO:  Have this test that manual axes are being used
@@ -66,31 +64,23 @@ public:
 
 private:
 
-   bool _normalized;
-   int _N;
    mutable Njettiness _njettinessFinder; // should muck with this so result can be const without this mutable
+
+   int _N;
+   bool _normalized;
 
 };
 
 inline Nsubjettiness::Nsubjettiness(int N, Njettiness::AxesMode mode, double beta, double R0, double Rcutoff, bool normalized)
   : _njettinessFinder(mode, NsubParameters(beta, R0, Rcutoff)), _N(N), _normalized(normalized) {}
 
-inline Double32_t Nsubjettiness::d0(const PseudoJet& jet) const {
-   std::vector<fastjet::PseudoJet> particles = jet.constituents();
-   return _njettinessFinder.getTau_denominator(particles); //calls on Njettiness getTau_denominator, which is calculated in MeasureFunctor
-   //Double32_t tau = 0.0;
-   //for (int i = 0; i < particles.size(); i++) {
-      //tau += particles[i].perp() * std::pow(_R0, _beta);
-   //}
-   //return tau;
-}
 
 //result will allow user to choose whether they want numerator or normalized version --TJW
 inline Double32_t Nsubjettiness::result(const PseudoJet& jet) const
 {
    std::vector<fastjet::PseudoJet> particles = jet.constituents();
    if (_normalized) return _njettinessFinder.getTau(_N, particles); 
-   else return _njettinessFinder.getTau_numerator(_N, particles);
+   else return _njettinessFinder.getTauNumerator(_N, particles);
 } 
 
 //Class NsubjettinessRatios
