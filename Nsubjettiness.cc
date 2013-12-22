21334a9edc8c1e0586a4ccf87665f7a6bad9b7fc
TJ Wilkason <tj.wilkason@gmail.com>
2013-12-22 23:32:53 +0000

    moved function definitions from .hh file
    git-svn-id: https://svn.hepforge.org/fastjetsvn/contrib/contribs/Nsubjettiness/trunk@398 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- Nsubjettiness.cc
+++ Nsubjettiness.cc
@@ -21,6 +21,8 @@
 // along with this code. If not, see <http://www.gnu.org/licenses/>.
 //----------------------------------------------------------------------
 
+//UPDATES FROM TJW:
+
 #include "Nsubjettiness.hh"
 #include "Njettiness.hh"
 #include "NjettinessPlugin.hh"
@@ -29,8 +31,26 @@ FASTJET_BEGIN_NAMESPACE      // defined in fastjet/internal/base.hh
 
 namespace contrib{
 
-// This file is blank, since all of the code is contained in the header files.
-// TODO:  Allow for a fastjet contrib without a .cc file
+//constructor definition moved from Nsubjettiness.hh file, but maybe easier to define it in class definition? -- TJW
+Nsubjettiness::Nsubjettiness(int N, Njettiness::AxesMode mode, double beta, double R0, double Rcutoff, bool normalized)
+  : _njettinessFinder(mode, NsubParameters(beta, R0, Rcutoff)), _N(N), _normalized(normalized) {}
+
+//result moved from Nsubjettiness.hh file --TJW
+//result allows user to choose whether they want tau numerator or tau normalized
+Double32_t Nsubjettiness::result(const PseudoJet& jet) const {
+   std::vector<fastjet::PseudoJet> particles = jet.constituents();
+   if (_normalized) return _njettinessFinder.getTau(_N, particles); 
+   else return _njettinessFinder.getTauNumerator(_N, particles);
+} 
+
+//ratio moved from Nsubjettiness.hh file --TJW
+//ratio result allows user to choose two tau values to create ratio of tau_M/tau_N
+//changed return value from double to Double32_t to match Nsubjettiness class -- TJW
+Double32_t NsubjettinessRatio::result(const PseudoJet& jet) const {
+   double numerator = _nsub_numerator.result(jet);
+   double denominator = _nsub_denominator.result(jet);
+   return numerator/denominator;
+}
 
 } // namespace contrib
 
