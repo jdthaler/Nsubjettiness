0dd97105fe6b3ccf3a17e24b038826ffadd46616
TJ Wilkason <tj.wilkason@gmail.com>
2013-12-22 23:13:05 +0000

    moved function definitions to .cc file
    git-svn-id: https://svn.hepforge.org/fastjetsvn/contrib/contribs/Nsubjettiness/trunk@397 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- NjettinessPlugin.hh
+++ NjettinessPlugin.hh
@@ -102,7 +102,6 @@ inline const NjettinessExtras * njettiness_extras(const fastjet::ClusterSequence
    return extras;   
 }
 
-
 /// The Njettiness jet algorithm
 /**
  * An exclusive jet finder that identifies N jets; first N axes are found, then
@@ -154,51 +153,7 @@ private:
 
 };
 
-inline NjettinessPlugin::NjettinessPlugin(int N, Njettiness::AxesMode mode, double beta, double R0, double Rcutoff)
-  : _N(N), _njettinessFinder(mode, NsubParameters(beta, R0, Rcutoff))
-{}
-
-inline NjettinessPlugin::NjettinessPlugin(int N, NsubGeometricParameters paraGeo)
-  : _N(N), _njettinessFinder(paraGeo)
-{}
-
-
-inline std::string NjettinessPlugin::description() const {return "NJettiness";}
-
-inline void NjettinessPlugin::run_clustering(ClusterSequence& cs) const
-{
-   std::vector<fastjet::PseudoJet> particles = cs.jets();
-   _njettinessFinder.getTau(_N, particles);
-   std::vector<std::list<int> > partition = _njettinessFinder.getPartition(particles);
-
-   std::vector<fastjet::PseudoJet> jet_indices_for_extras;
-
-   // output clusterings for each jet
-   for (size_t i = 0; i < partition.size(); ++i) {
-      std::list<int>& indices = partition[i];
-      if (indices.size() == 0) continue;
-      std::list<int>::const_iterator it = indices.begin();
-      while (indices.size() > 1) {
-         int merge_i = indices.back(); indices.pop_back();
-         int merge_j = indices.back(); indices.pop_back();
-         int newIndex;
-         double fakeDij = -1.0;
-      
-         cs.plugin_record_ij_recombination(merge_i, merge_j, fakeDij, newIndex);
-
-         indices.push_back(newIndex);
-      }
-      double fakeDib = -1.0;
-      
-      int finalJet = indices.back();
-      cs.plugin_record_iB_recombination(finalJet, fakeDib);
-      jet_indices_for_extras.push_back(cs.jets()[finalJet]);  // Get the four vector for the final jets to compare later.
-   }
-
-   NjettinessExtras * extras = new NjettinessExtras(_njettinessFinder.currentTau(),jet_indices_for_extras,_njettinessFinder.currentTaus(),_njettinessFinder.currentAxes());
-   cs.plugin_associate_extras(std::auto_ptr<ClusterSequence::Extras>(extras));
-   
-}
+//definition of functions moved over to NjettinessPlugin.cc -- TJW
 
 } // namespace contrib
 
