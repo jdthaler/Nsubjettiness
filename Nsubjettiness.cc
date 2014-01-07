0b8873d791877c16ecdd95d6001d8587f1ea13b1
TJ Wilkason <tj.wilkason@gmail.com>
2014-01-07 20:45:10 +0000

    updated copyright info
    git-svn-id: https://svn.hepforge.org/fastjetsvn/contrib/contribs/Nsubjettiness/trunk@424 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- Nsubjettiness.cc
+++ Nsubjettiness.cc
@@ -1,8 +1,8 @@
 //  Nsubjettiness Package
 //  Questions/Comments?  jthaler@jthaler.net
 //
-//  Copyright (c) 2011-13
-//  Jesse Thaler, Ken Van Tilburg, and Christopher K. Vermilion
+//  Copyright (c) 2011-14
+//  Jesse Thaler, Ken Van Tilburg, Christopher K. Vermilion, and TJ Wilkason
 //
 //----------------------------------------------------------------------
 // This file is part of FastJet contrib.
@@ -21,31 +21,27 @@
 // along with this code. If not, see <http://www.gnu.org/licenses/>.
 //----------------------------------------------------------------------
 
-//UPDATES FROM TJW:
-
 #include "Nsubjettiness.hh"
-#include "Njettiness.hh"
-#include "NjettinessPlugin.hh"
 
 FASTJET_BEGIN_NAMESPACE      // defined in fastjet/internal/base.hh
 
 namespace contrib{
 
-//constructor definition moved from Nsubjettiness.hh file, but maybe easier to define it in class definition? -- TJW
-Nsubjettiness::Nsubjettiness(int N, Njettiness::AxesMode mode, double beta, double R0, double Rcutoff, bool normalized)
-  : _njettinessFinder(mode, NsubParameters(beta, R0, Rcutoff)), _N(N), _normalized(normalized) {}
+//constructor definition moved to class definition in Nsubjettiness.hh -- TJW 12/25
 
-//result moved from Nsubjettiness.hh file --TJW
-//result allows user to choose whether they want tau numerator or tau normalized
+//result moved from Nsubjettiness.hh file -- TJW 12/22
+
+//result returns tau_N with normalization dependent on what is specified in constructor -- comment added by TJW
 Double32_t Nsubjettiness::result(const PseudoJet& jet) const {
    std::vector<fastjet::PseudoJet> particles = jet.constituents();
    if (_normalized) return _njettinessFinder.getTau(_N, particles); 
    else return _njettinessFinder.getTauNumerator(_N, particles);
 } 
 
-//ratio moved from Nsubjettiness.hh file --TJW
-//ratio result allows user to choose two tau values to create ratio of tau_M/tau_N
-//changed return value from double to Double32_t to match Nsubjettiness class -- TJW
+//ratio result moved from Nsubjettiness.hh file -- TJW 12/22
+//changed return value from double to Double32_t to match Nsubjettiness class -- TJW 12/22
+
+//ratio result uses Nsubjettiness result to find the ratio tau_N/tau_M, where N and M are specified by user -- comment added by TJW
 Double32_t NsubjettinessRatio::result(const PseudoJet& jet) const {
    double numerator = _nsub_numerator.result(jet);
    double denominator = _nsub_denominator.result(jet);
