58eebc4a16048454b54b4a4a2c0732386516ea75
TJ Wilkason <tj.wilkason@gmail.com>
2015-07-22 16:54:37 +0000

    fixing warning, improving combinatorial axes loop
    git-svn-id: https://svn.hepforge.org/fastjetsvn/contrib/contribs/Nsubjettiness/trunk@829 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- AxesDefinition.hh
+++ AxesDefinition.hh
@@ -284,18 +284,19 @@ public:
 
       else {
 
-        // define string of 1's based on number of desired jets
-        std::string bitmask(n_jets, 1);
-        // expand the array size to the total number of jets with extra 0's at the end, makes string easy to permute
-        bitmask.resize(starting_number, 0); 
+        // create vector of ascending numbers to easily permute
+        std::vector<int> index_vector(starting_number);
+        for (int i = 0; i < starting_number; i++) {index_vector[i] = i;}
 
         double min_tau = std::numeric_limits<double>::max();
+        std::vector<fastjet::PseudoJet> temp_axes;
+        
         do {
-          std::vector<fastjet::PseudoJet> temp_axes;
+          temp_axes.clear();
 
-          // only take an axis if it is listed as true (1) in the string
-          for (int i = 0; i < (int)starting_axes.size(); ++i) {
-            if (bitmask[i]) temp_axes.push_back(starting_axes[i]);
+          // take the first n_jet axes in the permuted vector
+          for (int i = 0; i < n_jets; ++i) {
+            temp_axes.push_back(starting_axes[index_vector[i]]);
           }
 
           double temp_tau = measure->result(inputs, temp_axes);
@@ -304,10 +305,11 @@ public:
             final_axes = temp_axes;
           }
 
-          // permutes string of 1's and 0's according to next lexicographic ordering and returns true
+          // permutes string of integers according to next lexicographic ordering and returns true
           // continues to loop through all possible lexicographic orderings
           // returns false and breaks the loop when there are no more possible orderings
-        } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
+
+        } while (std::next_permutation(index_vector.begin(), index_vector.end()));
       }
 
       return final_axes;
@@ -347,7 +349,7 @@ public:
       fastjet::ClusterSequence jet_clust_seq(inputs, _def);
       std::vector<fastjet::PseudoJet> myJets = sorted_by_pt(jet_clust_seq.inclusive_jets());
       
-      if (myJets.size() < n_jets) {
+      if ((int)myJets.size() < n_jets) {
          _too_few_axes_warning.warn("HardestJetAxes::get_starting_axes:  Fewer than N axes found; results are unpredicatable.");
       }
       
